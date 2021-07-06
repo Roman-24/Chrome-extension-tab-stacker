63f03f09dca9d057d49d19224aa81e7a867fa44f
Roman Bitarovský <bitarovskyroman@gmail.com>
2021-07-06 10:41:43 +0200

    komentáre, ver 1.0 final

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+
 const inputButton = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const binTabButton = document.getElementById("tab-btn")
 const deleteButton = document.getElementById("delete-btn")
+
 let myLeads = []
 
 const regex = new RegExp('^(http|https)://')
@@ -15,8 +17,11 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
+
 inputButton.addEventListener("click", function () {
 
+    // if manual input link does not have https part 
+    // add https part
     let myInput = inputEl.value
     if (myInput.search(regex) == -1){
         myInput = "https://" + myInput
@@ -33,11 +38,13 @@ inputButton.addEventListener("click", function () {
     render(myLeads)
 })
 
+
 deleteButton.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
+
 
 // bin active tab to myLeads
 binTabButton.addEventListener("click", function () {
@@ -50,15 +57,18 @@ binTabButton.addEventListener("click", function () {
     })
 })
 
+
 function render(leads) {
     let listItems = ""
 
     // creating list with input links to websites
     for (let i in leads) {
 
+        // cleaning url for <a> content
         let myUrl = leads[i].replace(regex, "")
         myUrl = myUrl.replace(regex2, "")
 
+        // creating string for ulEl.innerHTML
         listItems += `<li>
                             <a href="${leads[i]}" target="_blank"> 
                                 ${myUrl}
